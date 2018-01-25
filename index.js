4bca5ca2857a8ee0ebb5b194a184f041649175cc
zoe chodosh <zoe.chodosh@nymag.com>
2018-01-25 11:34:10 -0500

    file structure does NOT need to be this complex

--- index.js
+++ index.js
@@ -1,3 +1,49 @@
 'use strict';
 
-module.exports.render = require('./lib/render');
+const _ = require('lodash'),
+  path = require('path'),
+  files = require('nymag-fs');
+
+function getSiteConfig(site) {
+  const ymlPath = path.resolve(site.dir, 'anf.yml').replace('.yml', '');
+
+  return files.getYaml(ymlPath);
+}
+
+function render(data) {
+  const article = Object.assign({}, data._data, getSiteConfig(data.site));
+
+  article.components = [];
+  article.version = '1.5';
+  article.language = 'en';
+  article.layout = {
+    columns: 10,
+    width: 1024,
+    margin: 85,
+    gutter: 20
+  };
+
+  /*
+   * we only want to add components that have been formatted for apple news
+   * the easiest way to tell this is if the component has a 'role' property
+   * if the component doesn't have a role, check for the 'multi' property
+   * some single clay components translate to multiple apple news components,
+   * in this case the component's anf renderer returns an object with the multi prop
+   * with an array of formatted anf components
+   */
+
+  _.forEach(article.content, (item) => {
+    if (item.role) {
+      article.components.push(_.omit(item, '_ref'));
+    } else if (item.multi) {
+      _.forEach(item.components, (cmpt) => article.components.push(cmpt));
+    }
+  });
+
+  return {
+    output: _.omit(article, 'content'),
+    type: 'json'
+  }
+}
+
+module.exports.render = render;
